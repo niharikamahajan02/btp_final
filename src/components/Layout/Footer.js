4a0ff16ea60305c8d50d9c2f4d5cb939b5e7919e
anujatiwari <anujatiwari13@gmail.com>
2023-03-30 01:02:02 +0530

    background and components chnaged

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,14 +1,12 @@
 import React from "react";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import YouTubeIcon from "@mui/icons-material/YouTube";
+
+
 import { Box, Typography } from "@mui/material";
 const Footer = () => {
   return (
     <>
       <Box
-        sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
+        sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 1 }}
       >
         <Box
           sx={{
@@ -25,11 +23,8 @@ const Footer = () => {
             },
           }}
         >
-          {/* icons */}
-          <InstagramIcon />
-          <TwitterIcon />
-          <GitHubIcon />
-          <YouTubeIcon />
+         
+          
         </Box>
         <Typography
           variant="h5"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
 import { Link } from "react-router-dom";
-import Banner from "../images/banner.jpeg";
+import Banner from "../images/bh.jpeg";
 import "../styles/HomeStyles.css";
 
 const Home = () => {
